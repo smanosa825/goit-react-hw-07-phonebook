e1c7dda264cec18c566b3bd9ba066faf6c96b501
sherwinmanosa <145818525+sherwinmanosa@users.noreply.github.com>
2024-07-31 19:52:20 +0800

    initial

---
+++ src/redux/store.js
@@ -0,0 +1,11 @@
+// store.js
+import { configureStore } from '@reduxjs/toolkit';
+import { contactsSlice } from './contactsSlice';
+import { filterSlice } from './filterSlice';
+
+export const store = configureStore({
+  reducer: {
+    contacts: contactsSlice.reducer,
+    filter: filterSlice.reducer,
+  },
+});
